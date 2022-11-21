9af6606248ec8ff8ecb6aa2c56f7d43eb03a8af6
liuliyi <wyang@shgbit.com>
2022-11-21 17:40:26 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Author: Sapphire Liu
+ * @Date: 2022-09-16 15:00:11
+ * @LastEditors: Sapphire Liu
+ * @LastEditTime: 2022-09-30 16:59:23
+ * @Description: index.js...
+ */
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react"; // 框架核心
+import ReactDOM from "react-dom"; // DOM渲染
+import "./index.css"; // 全局样式文件
+import App from "./App"; // 引入根组件
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  // 严格节点模式需要去掉，这会影响到useEffect的执行时机
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+); // 渲染根组件App到一个id为root到节点上
